6cb0d7282817ae714e6b0a3423568985b247f9f9
starryeye <see2818@naver.com>
2022-05-31 23:48:30 +0900

    [starryeye, 2022.05.31] - 변수명 정리 내용  - 변수명 정리 개선  -

--- goodPlace/goodPlace/src/main/java/com/example/goodPlace/favorRepository/FavorPlaceEntity.java
+++ goodPlace/goodPlace/src/main/java/com/example/goodPlace/favorRepository/FavorPlaceEntity.java
@@ -12,16 +12,16 @@ import java.time.LocalDateTime;
 @Data
 public class FavorPlaceEntity extends PlaceEntity {
 
-    private String m_sCategory;            // 즐겨찾는 장소 카테고리
-    private String m_sTitle;               // 장소명
+    private String Category;            // 즐겨찾는 장소 카테고리
+    private String Title;               // 장소명
 
-    private String m_sHomePage;            // 홈페이지 주소
-    private String m_sImage;               // 이미지
+    private String HomePage;            // 홈페이지 주소
+    private String Image;               // 이미지
 
-    private String m_sAddress;             // 주소
-    private String m_sRoadAddress;         // 도로명 주소
+    private String Address;             // 주소
+    private String RoadAddress;         // 도로명 주소
 
-    private int m_iVisit;                  // 방문 횟수
-    private boolean m_bVisited;            // 방문 유무
-    private LocalDateTime m_RecentVisit;   // 최근 방문 날짜
+    private int Visit;                  // 방문 횟수
+    private boolean Visited;            // 방문 유무
+    private LocalDateTime RecentVisit;   // 최근 방문 날짜
 }
